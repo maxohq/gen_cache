8e9b03b20af1f62ca7715024c09f8b34805f5fbd
Roman Heinrich <roman.heinrich@gmail.com>
2024-09-06 19:26:41 +0200

    Init project

---
+++ lib/gen_cache.ex
@@ -0,0 +1,175 @@
+defmodule GenCache do
+  @moduledoc """
+  Generic cache layer for anything.
+  Allows concurrent requests without work duplication and blocking.
+
+  Usage:
+  ```
+  {:ok, pid} = GenCache.start_link()
+
+  # populate cache with default `expire_in` value
+  response = GenCache.request(pid, {Mod, :fun, [arg1, arg2]})
+
+  # populate cache with custom `expire_in` value
+  response = GenCache.request(pid, {Mod, :fun, [arg1, arg2]}, expire_in: :timer.seconds(15))
+  ```
+
+  Special notes:
+  - we use a map to signify the state of the cache (usually this is an atom)
+  - this map contains currently running cache misses
+  - every change on this map triggers a state transition in gen_statem
+  - the postponed requests get a chance to run again on each state transition
+  """
+
+  @behaviour :gen_statem
+
+  @default_expire_in :timer.seconds(30)
+
+  defstruct busy: %{}, cache: %{}, valid_until: %{}, expire_in: %{}
+
+  def start_link(opts \\ []), do: :gen_statem.start_link(__MODULE__, [], opts)
+
+  @impl true
+  def callback_mode(), do: [:handle_event_function, :state_enter]
+
+  @impl true
+  def init(_) do
+    # state / data / actions
+    {:ok, %{}, %__MODULE__{}, []}
+  end
+
+  ### PUBLIC API ###
+
+  def request(pid, request, opts \\ []), do: :gen_statem.call(pid, {:request, request, opts})
+  def remove(pid, request), do: :gen_statem.call(pid, {:remove, request})
+  def get_state(pid), do: :gen_statem.call(pid, :get_state)
+
+  ### INTERNAL ###
+
+  @impl :gen_statem
+  def handle_event(:enter, _before_state, _after_state, _data), do: {:keep_state_and_data, []}
+
+  # just return state / data
+  def handle_event({:call, from}, :get_state, _state, data) do
+    {:keep_state, data, [{:reply, from, data}]}
+  end
+
+  def handle_event({:call, from}, {:remove, request}, _state, data) do
+    data =
+      remove_from_cache(data, request)
+      |> remove_valid_until(request)
+      |> remove_expire_in(request)
+
+    {:keep_state, data, [{:reply, from, :ok}]}
+  end
+
+  #
+  def handle_event({:call, from}, {:request, request, req_opts}, _, data) do
+    expire_in = Keyword.get(req_opts, :expire_in, @default_expire_in)
+    data = update_expire_in(data, request, expire_in)
+    {res, data} = get_from_cache(data, request)
+    is_busy = is_busy_for_request(data, request)
+
+    cond do
+      # we have a result in cache, so we reply immediately
+      res != nil ->
+        actions = [{:reply, from, res}]
+        {:keep_state, data, actions}
+
+      # we are already busy with this request, so we postpone
+      is_busy ->
+        actions = [:postpone]
+        {:keep_state_and_data, actions}
+
+      # not in cache and no in-progress fetching, so we schedule a fetch
+      true ->
+        data = mark_busy_for_request(data, request, from)
+        actions = [{:next_event, :internal, {:fetch_data, request, from}}]
+        {:next_state, data.busy, data, actions}
+    end
+  end
+
+  # fetch data and populate the cache
+  def handle_event(:cast, {:set_response, request, response, from}, _state, data) do
+    data = mark_done_for_request(data, request)
+    data = store_in_cache(data, request, response)
+    data = update_valid_until(data, request)
+    actions = [{:reply, from, response}]
+    {:next_state, data.busy, data, actions}
+  end
+
+  def handle_event(:internal, {:fetch_data, {mod, fun, args} = request, from}, _s, _data) do
+    pid = self()
+
+    # run the fetch in a separate process, to unblock our main loop
+    Task.start(fn ->
+      response = apply(mod, fun, args)
+      GenServer.cast(pid, {:set_response, request, response, from})
+    end)
+
+    {:keep_state_and_data, []}
+  end
+
+  defp is_busy_for_request(data, request) do
+    Map.get(data.busy, request, false)
+  end
+
+  defp mark_busy_for_request(data, request, from) do
+    %__MODULE__{data | busy: Map.put(data.busy, request, from)}
+  end
+
+  defp mark_done_for_request(data, request) do
+    %__MODULE__{data | busy: Map.delete(data.busy, request)}
+  end
+
+  defp store_in_cache(data, request, res) do
+    %__MODULE__{data | cache: Map.put(data.cache, request, res)}
+  end
+
+  defp remove_from_cache(data, request) do
+    %__MODULE__{data | cache: Map.delete(data.cache, request)}
+  end
+
+  defp get_from_cache(data, request) do
+    res = Map.get(data.cache, request, nil)
+
+    if res do
+      {res, update_valid_until(data, request)}
+    else
+      {nil, data}
+    end
+  end
+
+  def update_valid_until(data, request) do
+    time = :erlang.monotonic_time()
+    expire_in = Map.get(data.expire_in, request, @default_expire_in)
+
+    %__MODULE__{
+      data
+      | valid_until: Map.put(data.valid_until, request, time + expire_in * 1_000_000)
+    }
+  end
+
+  def remove_valid_until(data, request) do
+    %__MODULE__{data | valid_until: Map.delete(data.valid_until, request)}
+  end
+
+  def update_expire_in(data, request, expire_in) do
+    %__MODULE__{data | expire_in: Map.put(data.expire_in, request, expire_in)}
+  end
+
+  def remove_expire_in(data, request) do
+    %__MODULE__{data | expire_in: Map.delete(data.expire_in, request)}
+  end
+
+  def remove_expired_entries(data, now) do
+    expired_keys = Enum.filter(data.valid_until, fn {_, v} -> v < now end)
+
+    Enum.reduce(expired_keys, data, fn {k, _time}, acc ->
+      acc
+      |> remove_from_cache(k)
+      |> remove_valid_until(k)
+      |> remove_expire_in(k)
+    end)
+  end
+end
---
+++ lib/gen_cache_test.exs
@@ -0,0 +1,138 @@
+defmodule GenCacheTest do
+  use ExUnit.Case
+  doctest GenCache
+
+  defmodule ReqBackend do
+    def fetch(request) do
+      Process.sleep(20)
+      "RESULT: #{inspect(request)}"
+    end
+  end
+
+  def req_tuple(value) do
+    {ReqBackend, :fetch, [value]}
+  end
+
+  describe "full run" do
+    test "multiple tasks with same request get the same result" do
+      {:ok, pid} = GenCache.start_link([])
+
+      tasks =
+        for _ <- 1..5 do
+          Task.async(fn ->
+            GenCache.request(pid, req_tuple(1))
+          end)
+        end
+
+      results = Task.await_many(tasks)
+
+      assert length(results) == 5
+      [first_result | rest] = results
+      assert Enum.all?(rest, &(&1 == first_result))
+    end
+
+    test "multiple tasks with different requests work fine" do
+      {:ok, pid} = GenCache.start_link([])
+
+      # we populate the cache
+      pop_tasks =
+        for _ <- 1..10 do
+          Task.async(fn ->
+            GenCache.request(pid, req_tuple(:rand.uniform(3)))
+          end)
+        end
+
+      fetch_tasks =
+        for _ <- 1..5 do
+          Task.async(fn ->
+            GenCache.request(pid, req_tuple(1))
+            GenCache.request(pid, req_tuple(2))
+            GenCache.request(pid, req_tuple(3))
+          end)
+        end
+
+      Task.await_many(pop_tasks ++ fetch_tasks)
+
+      assert GenCache.request(pid, req_tuple(1)) == "RESULT: 1"
+      state = clean_state(pid)
+
+      assert state ==
+               %GenCache{
+                 busy: %{},
+                 cache: %{
+                   {GenCacheTest.ReqBackend, :fetch, [1]} => "RESULT: 1",
+                   {GenCacheTest.ReqBackend, :fetch, [2]} => "RESULT: 2",
+                   {GenCacheTest.ReqBackend, :fetch, [3]} => "RESULT: 3"
+                 },
+                 expire_in: %{
+                   {GenCacheTest.ReqBackend, :fetch, [1]} => 30000,
+                   {GenCacheTest.ReqBackend, :fetch, [2]} => 30000,
+                   {GenCacheTest.ReqBackend, :fetch, [3]} => 30000
+                 }
+               }
+    end
+  end
+
+  describe "remove_cache" do
+    test "works" do
+      {:ok, pid} = GenCache.start_link([])
+      GenCache.request(pid, req_tuple(1))
+      GenCache.request(pid, req_tuple(2))
+      GenCache.remove(pid, req_tuple(1))
+
+      assert clean_state(pid) == %GenCache{
+               busy: %{},
+               expire_in: %{
+                 {GenCacheTest.ReqBackend, :fetch, [2]} => 30000
+               },
+               cache: %{{GenCacheTest.ReqBackend, :fetch, [2]} => "RESULT: 2"}
+             }
+    end
+  end
+
+  describe "remove_expired" do
+    test "works" do
+      {:ok, pid} = GenCache.start_link([])
+
+      GenCache.request(pid, req_tuple(1), expire_in: :timer.seconds(5))
+      GenCache.request(pid, req_tuple(2), expire_in: :timer.seconds(10))
+      GenCache.request(pid, req_tuple(3), expire_in: :timer.seconds(15))
+      # default expire_in - 30 seconds
+      GenCache.request(pid, req_tuple(4))
+
+      now = :erlang.monotonic_time()
+
+      data = GenCache.get_state(pid)
+      time_factor = 1_000_000
+
+      data1 = GenCache.remove_expired_entries(data, now + :timer.seconds(5) * time_factor)
+
+      assert data1.cache == %{
+               {GenCacheTest.ReqBackend, :fetch, [2]} => "RESULT: 2",
+               {GenCacheTest.ReqBackend, :fetch, [3]} => "RESULT: 3",
+               {GenCacheTest.ReqBackend, :fetch, [4]} => "RESULT: 4"
+             }
+
+      data2 = GenCache.remove_expired_entries(data, now + :timer.seconds(10) * time_factor)
+
+      assert data2.cache == %{
+               {GenCacheTest.ReqBackend, :fetch, [3]} => "RESULT: 3",
+               {GenCacheTest.ReqBackend, :fetch, [4]} => "RESULT: 4"
+             }
+
+      data3 = GenCache.remove_expired_entries(data, now + :timer.seconds(15) * time_factor)
+
+      assert data3.cache == %{
+               {GenCacheTest.ReqBackend, :fetch, [4]} => "RESULT: 4"
+             }
+
+      data4 = GenCache.remove_expired_entries(data, now + :timer.seconds(30) * time_factor)
+
+      assert data4.cache == %{}
+    end
+  end
+
+  def clean_state(pid) do
+    GenCache.get_state(pid) |> Map.put(:valid_until, %{})
+  end
+end
