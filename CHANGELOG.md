9112090606283e8d04c5ec91b27dbc41efa8d56a
Roman Heinrich <roman.heinrich@gmail.com>
2024-09-06 23:40:23 +0200

    Chore: prepare first release

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## v0.1.0 (2024-09-06)
+
+### First release
+
+- already quite usable, there might still be some missing  features
--- mix.exs
+++ mix.exs
@@ -1,15 +1,23 @@
 defmodule GenCache.MixProject do
   use Mix.Project
 
+  @github_url "https://github.com/maxohq/gen_cache"
+  @version "0.1.0"
+  @description "gen_statem based generic cache with MFA-based keys"
+
   def project do
     [
       app: :gen_cache,
-      version: "0.1.0",
+      source_url: @github_url,
+      version: @version,
+      description: @description,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      package: package(),
       test_paths: ["lib"],
-      test_pattern: "*_test.exs"
+      test_pattern: "*_test.exs",
+      docs: [extras: ["README.md"]]
     ]
   end
 
@@ -21,11 +29,21 @@ defmodule GenCache.MixProject do
     ]
   end
 
+  defp package do
+    [
+      files: ~w(lib mix.exs README* CHANGELOG* LICENCE*),
+      licenses: ["MIT"],
+      links: %{
+        "Github" => @github_url,
+        "Changelog" => "#{@github_url}/blob/main/CHANGELOG.md"
+      }
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.29", only: :dev, runtime: false}
     ]
   end
 end
